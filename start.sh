b47f3a871b651749da98f283adb9252de9178f98
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2022-10-12 23:26:35 -0400

    feat: initial commit for script that create a default settings to linux instances

---
+++ start.sh
@@ -0,0 +1,58 @@
+echo "Removendo configurações anteriores"
+
+rm -vfr /publico
+rm -vfr /adm
+rm -vfr /ven
+rm -vfr /sec
+
+userdel -r carlos
+userdel -r maria
+userdel -r joao_
+userdel -r debora
+userdel -r sebastiana
+userdel -r roberto
+userdel -r josefina
+userdel -r amanda
+userdel -r rogerio
+
+groupdel GRP_ADM
+groupdel GRP_VEN
+groupdel GRP_SEC
+
+echo "Criando estrutura de diretórios"
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos para usuários no sistema"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários do sistema"
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao_ -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Configurando as permissões das pastas: /publico, /adm, /ven, /sec"
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
